5c5e3764350dd9814e3a096f57e54dea90d74f68
Mikka Akdag <mia011@edu.zealand.dk>
2025-08-27 12:23:35 +0200

    laver trin 1 og 2

---
+++ ClassRoomNet60/Studerende.cs
@@ -0,0 +1,18 @@
+﻿public class Studerende
+{
+    public string Navn {  get; set; }
+    public int FødselsMåned { get; set; }
+    public int Fødselsdag {  get; set; }
+
+    public Studerende (string navn, int fødselsMåned, int fødselsdag)
+    {
+        Navn = navn;
+        FødselsMåned = fødselsMåned;
+        Fødselsdag = fødselsdag;
+    }
+
+
+
+
+
+}
